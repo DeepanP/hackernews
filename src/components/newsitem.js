262c781d173c5e79fcaa64d517d10fc38d0d0476
Deepan Palaniyappan <deepan.palaniyappan@sapient.com>
2020-05-14 21:50:46 +0530

    store and action creatrions

---
+++ src/components/newsitem.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const NewsItem = ()=>{
+  return (
+    <div className='wrapper'>
+      <div className='comments'></div>
+      <div className='upvotes'>
+      </div>
+      <div className='title'>feed title</div>
+      <details className='info-mobile'>
+        <summary>details</summary>
+        <div className='domain'>domain</div>
+        <div className='by-text'>by</div>
+        <div className='author'>author</div>
+        <div className='time'>time</div>
+      </details>
+        <div className='info-desktop'>
+          <div className='domain'>domain</div>
+          <div className='by-text'>by</div>
+          <div className='author'>author</div>
+          <div className='time'>time</div>
+        </div>
+      <div className='hide-action'>[<span className='hide-text'>hide</span>]</div>
+    </div>
+  );
+}
+
+export default NewsItem;
---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+
+import { GET_NEWS } from '../actions';
+
+export default combineReducers({
+    news: (state=[], actions) => {
+
+        switch(actions.type) {
+            case GET_NEWS:
+                return actions.payload
+            default:
+                return state;
+        }
+        
+    }
+})
